bf181bcd9592f52b478ddad0dea4583abd2450b6
miraodasilva <rs2517@ic.ac.uk>
2022-08-03 17:03:11 +0000

    generate file names for teacher forcing properly (without _generated)

--- inference.py
+++ inference.py
@@ -57,7 +57,7 @@ def inference(a):
             audio = audio * MAX_WAV_VALUE
             audio = audio.cpu().numpy().astype('int16')
 
-            output_file = os.path.join(a.output_dir, os.path.splitext(filname)[0] + '_generated.wav')
+            output_file = os.path.join(a.output_dir, os.path.splitext(filname)[0] + '.wav')
             write(output_file, h.sampling_rate, audio)
             print(output_file)
 
